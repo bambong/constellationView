4c3bd66197ba8907d244722cd48c814a45287d60
bambong97 <56501771+bambong97@users.noreply.github.com>
2020-05-24 19:26:31 +0900

    코드정리

--- Assets/Script/CameraObrbit.cs
+++ Assets/Script/CameraObrbit.cs
@@ -3,37 +3,41 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class CameraObrbit : MonoBehaviour
-{
-    public float MinDistance = -1000f;
-    public float MaxDistance = 130f;
-    float distance = 1000;
-    float distanceTarget;
-    Vector2 mouse;
-    Vector2 mouseOnDown;
-    Vector2 rotation;
-    Vector2 target = new Vector2(Mathf.PI * 3 / 2, Mathf.PI / 6);
-    Vector2 targetOnDown;
+{   
+    [SerializeField]
+    private float MinDistance = -1000f;
+    [SerializeField]
+    private float MaxDistance = 130f;
+    private float distance = 1000;
+    private float distanceTarget;
+    private Vector2 mouse;
+    private Vector2 mouseOnDown;
+    private Vector2 rotation;
+    private Vector2 target = new Vector2(Mathf.PI * 3 / 2, Mathf.PI / 6);
+    private Vector2 targetOnDown;
 
-    public float speed = 25.0f;
-    public Transform cameraTarget;
+    [SerializeField]
+    private float speed = 25.0f;
+    [SerializeField]
+    private Transform cameraTarget;
+
     private Camera thisCamera;
     private Vector3 worldDefalutForward;
 
 
-    // Use this for initialization
-    float time = 0;
-    float fades = 0.0f;
-    
+    private bool down = false;
+    private bool focusOn = false;
 
-    void Start () {
+
+
+    void Start()
+    {
+
         distanceTarget = transform.position.magnitude;
         thisCamera = GetComponent<Camera>();
         worldDefalutForward = transform.forward;
-        
+
     }
-    bool down = false;
-    bool focusOn = false; 
-    
     // Update is called once per frame
 
     void ZoomInput()
@@ -47,52 +51,8 @@ public class CameraObrbit : MonoBehaviour
         thisCamera.fieldOfView += scroll;
     } // 줌인 줌아웃 input
 
-
-    void FocusCheck()
+    void DragInput()
     {
-
-        if (!focusOn)
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            int layerMask = 1 << LayerMask.NameToLayer("Constellation");
-
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-            {
-
-                Debug.Log("FocusOn");
-                StartCoroutine(FocusConstellation(hit));
-                focusOn = true;
-            }
-           
-
-        } 
-
-    }
-    IEnumerator FocusConstellation(RaycastHit focustarget)
-    {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-        int layerMask = 1 << LayerMask.NameToLayer("Constellation");
-        Constellation temp = focustarget.collider.GetComponent<Constellation>();
-        temp.FadeStart();
-
-
-        while (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-        { 
-            ray = Camera.main.ScreenPointToRay(Input.mousePosition);       
-            yield return null;
-                      
-        }
-
-        Debug.Log("FocusOff");
-        temp.FadeReset();
-        focusOn = false;
-
-    }
-    void Update()
-    {
-
 #if UNITY_ANDROID || UNITY_IPHONE
 
         if (Input.touchCount > 0) // 모바일입력
@@ -112,25 +72,23 @@ public class CameraObrbit : MonoBehaviour
                 down = false;
             }
         }
-
-
-      
+    
 
 #else
 
-        if (Input.GetMouseButtonDown(0)) 
-            {
-                down = true;
-                mouseOnDown.x = Input.mousePosition.x;
-                mouseOnDown.y = -Input.mousePosition.y;
+        if (Input.GetMouseButtonDown(0))
+        {
+            down = true;
+            mouseOnDown.x = Input.mousePosition.x;
+            mouseOnDown.y = -Input.mousePosition.y;
 
-                targetOnDown.x = target.x;
-                targetOnDown.y = target.y;
-            }
-            else if (Input.GetMouseButtonUp(0))
-            {
-                down = false;
-            }
+            targetOnDown.x = target.x;
+            targetOnDown.y = target.y;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            down = false;
+        }
 
 
 #endif
@@ -156,9 +114,6 @@ public class CameraObrbit : MonoBehaviour
         }
 
 
-        ZoomInput();
-
-
         distanceTarget -= Input.GetAxis("Mouse ScrollWheel");
         distanceTarget = Mathf.Clamp(distanceTarget, MinDistance, MaxDistance);
 
@@ -172,38 +127,56 @@ public class CameraObrbit : MonoBehaviour
         transform.position = position;
         transform.LookAt(Vector3.zero);
 
+    }
 
+    void FocusCheck()
+    {
+
+        if (!focusOn)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            int layerMask = 1 << LayerMask.NameToLayer("Constellation");
+
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+            {
+
+                Debug.Log("FocusOn");
+                StartCoroutine(FocusConstellation(hit));
+                focusOn = true;
+            }
+           
+
+        } 
+
+    }
+
+    IEnumerator FocusConstellation(RaycastHit focustarget)
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
         int layerMask = 1 << LayerMask.NameToLayer("Constellation");
-        SpriteRenderer render;
+        Constellation temp = focustarget.collider.GetComponent<Constellation>();
+        temp.FadeStart();
 
 
+        while (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        { 
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);       
+            yield return null;
+                      
+        }
 
-        //RaycastHit hit;
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.Log("FocusOff");
+        temp.FadeReset();
+        focusOn = false;
 
-        ////if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-        ////{
+    }
 
-
-
-        ////    Debug.Log("hit");
-        ////    time += Time.deltaTime;
-        ////    render = hit.transform.GetComponent<SpriteRenderer>();
-        ////    if (fades < 1.0f && time >= 0.1f)
-        ////    {
-        ////        fades += 0.04f;
-        ////        time = 0;
-        ////        render.color = new Color(1, 1, 1, fades);
-        ////    }
-        ////}
-        ////else
-        ////{
-        ////    time = 0;
-        ////    fades = 0;
-        ////}
-        ///
-
+    void Update()
+    {
+        DragInput();
+        ZoomInput();
         FocusCheck();
-
     }
 }
